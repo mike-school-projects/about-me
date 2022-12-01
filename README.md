5ebd2250aa154856f8235a8c668ebc11424b2903
Mike Shen <michael.m.shen@gmail.com>
2022-12-01 20:26:21 +0000

    added lab 4

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Lab 2 creates a simple guessing game to learn 5 facts about me.
 
 Lab 3 adds 2 more games: a number guessing game and a multi-choice answer game.
 
+Lab 4 cleans it up and wraps all the logic into functions.  Also put all question/answer data into arrays.
+
 ## Author
 
 Mike Shen
--- js/app.js
+++ js/app.js
@@ -1,162 +1,157 @@
 'use strict';
 
 // Global variables
+let userName;
 let numCorrect = 0;
-let i = 0;
-let j = 0;
 
-// Basic info
-let userName = prompt('What is your name?');
-alert(`Welcome to my site ${userName}.`);
-
-// Guess 5 basic facts game: START OF SECTION *************
-// Guess 5 basic facts game: Variables
-let questionOne = 'Am I married?';
-let questionTwo = 'Do I have kids?';
-let questionThree = 'Am I older than 50?';
-let questionFour = 'Do I like Star Wars?';
-let questionFive = 'Was I born in Washington?';
-
-let questionOneAnswer = 'y';
-let questionTwoAnswer = 'n';
-let questionThreeAnswer = 'n';
-let questionFourAnswer = 'y';
-let questionFiveAnswer = 'n';
-
-let questionOneGuess = '0';
-let questionTwoGuess = '0';
-let questionThreeGuess = '0';
-let questionFourGuess = '0';
-let questionFiveGuess = '0';
-
-
-// Guess 5 basic facts game: Function calls
-alert('Please answer the following 5 questions with yes/no or y/n');
-ask(questionOne, questionOneGuess, questionOneAnswer);
-ask(questionTwo, questionTwoGuess, questionTwoAnswer);
-ask(questionThree, questionThreeGuess, questionThreeAnswer);
-ask(questionFour, questionFourGuess, questionFourAnswer);
-ask(questionFive, questionFiveGuess, questionFiveAnswer);
-
-// Guess 5 basic facts game: END OF SECTION ****************
-
-// Guessing Number Game: START OF SECTION ##################
-// Guessing Number Game: Variables
-let guessingGameAnswer = 7;
-let guessingGameGuess = 0;
-let guessingGameNumGuesses = 1;
-
-// Guessing Number Game: Initial user input
-alert('This is a number guessing game!  Guess a number between 1-10.  You have 4 chances to win!');
-
-for (guessingGameNumGuesses = 0; guessingGameNumGuesses < 4; guessingGameNumGuesses++) {
-  guessingGameGuess = prompt('What number am I thinking of?');
-  guessingGameGuess = parseInt(guessingGameGuess);
-
-  if (guessingGameGuess === guessingGameAnswer) {
-    alert(`Great!  ${guessingGameGuess} is correct!  You got it in ${guessingGameNumGuesses+1} guesses!`);
-    guessingGameNumGuesses = 10;
-    numCorrect++;
-  } else if (guessingGameGuess > guessingGameAnswer) {
-    alert('Too high.  Try again');
-  } else if (guessingGameGuess < guessingGameAnswer) {
-    alert('Too low.  Try again');
-  }
-
-  if ((guessingGameGuess !== guessingGameAnswer) && guessingGameNumGuesses === 3){
-    alert(`Sorry, you ran out of chances.  The number I was thinking of is ${guessingGameAnswer}`);
-  }
-
-}
-// Guessing Number Game: END OF SECTION ##################
-
-
-// Guessing Movie Game: START OF SECTION ##################
-// Guessing Movie Game: Local variables
-
-let movieList = [
-//  Answer      If correctly guessed
-  ['Star Wars', false],
-  ['Lord of the Rings', false],
-  ['The Matrix', false],
-];
-
-let movieGuess = [];
-let movieNumCorrect=0;
-
-// Guessing Movie Game: initial prompt
-alert('Guess my top 3 favorite movie series.  You have 6 chances');
-
-// First loop: 6 guesses
-for (i = 0; i < 6; i++) {
-  movieGuess[i] = prompt(`Guess #${i + 1}`);
-
-  // Second loop: compare against all 3 answers
-  for (j = 0; j < 3; j++) {
-    if (movieGuess[i] === movieList[j][0] && movieList[j][1] !== true) {
-      alert('Correct!');
-      movieNumCorrect++;
-      movieList[j][1] = true;
-      j = 3;
-    }
-  }
-
-  if (movieNumCorrect === 3){
-    i=6;
-  }
-
-}
-
-alert(`My favorite movie series are ${movieList[0][0]}, ${movieList[1][0]}, and ${movieList[2][0]}`);
-
-numCorrect += movieNumCorrect;
-
-// Guessing Movie Game: END OF SECTION ##################
-
-
-// Summary *********************************
-if (numCorrect >= 5){
-  alert(`${userName} - you got ${numCorrect} out of 9.  We must be best friends`);
-} else if (numCorrect > 2){
-  alert(`${userName} - you got ${numCorrect} out of 9.  You did okay.  Let's have some lunch and get to know each other better`);
-} else {
-  alert(`${userName} - you got ${numCorrect} out of 9.  Who are you again?`);
-}
-
-
-
+// Function Calls
+userName = intro();
+facts();
+number();
+movie();
+summary();
 
 // Functions ********************************
+// Get Basic Info function
+function intro(){
+  let name = prompt('What is your name?');
+  alert(`Welcome to my site ${name}.`);
+  return name;
+}
+
+
 // Guess 5 basic facts function
-function ask (question, guess, answer){
+function facts(){
+  let guess = 0;
   let i = 0;
-  while ((guess !== 'y') && (guess !== 'n') && (guess !== 'yes') && (guess !== 'no')) {
-    if (i === 0){
-      guess = prompt(question);
-    } else if(i < 4) {
-      guess = prompt(`Try again - ${question}`);
-    } else if(i > 9){
-      return;
-    } else {
-      guess = prompt(`Make sure you use either yes, no, y, n - ${question}`);
+  let j = 0;
+
+  const question = [
+    ['Am I married?','y'],
+    ['Do I have kids?','n'],
+    ['Am I older than 50?','n'],
+    ['Do I like Star Wars?','y'],
+    ['Was I born in Washington?','n']
+  ];
+
+  alert('Let us play a game about some basic facts about me.  Please answer the following 5 questions with yes/no or y/n');
+
+  for(i=0; i<5; i++){
+    // Get guess and validate data is yes, no, y, or n.  3 tries to get good input.
+    for(j=0; j<3; j++){
+      if(j===0){
+        guess = prompt(question[i][0]);
+        if (guess === null){
+          guess = 'a';
+        }
+        guess = guess.toLowerCase();
+      } else if((guess !== 'y') && (guess !== 'n') && (guess !== 'yes') && (guess !== 'no')){
+        guess = prompt(`Make sure you use either yes, no, y, n - ${question[i][0]}`);
+        if (guess === null){
+          guess = 'a';
+        }
+        guess = guess.toLowerCase();
+      }
     }
 
+    // normalize guess to y / n
     if (guess === null){
-      guess = 0;
+      guess = 'a';
     }
-    i++;
+    guess = guess[0].toLowerCase();
+
+    // Compare guess to answer
+    if (guess === question[i][1]) {
+      alert(`The answer to "${question[i][0]}" is correct!`);
+      // console.log(`The anwer to "${question}" is correct!`);
+      numCorrect++;
+    } else {
+      alert(`The answer to "${question[i][0]}" is incorrect.`);
+      // console.log(`The anwer to "${question}" is incorrect.`);
+    }
+  }
+}
+
+// Guessing Number Game FUNCTION
+// Guessing Number Game: Variables
+function number(){
+  let guessingGameAnswer = 7;
+  let guessingGameGuess = 0;
+  let i = 0;
+
+  // Guessing Number Game: Initial user input
+  alert('This is a number guessing game!  Guess a number between 1-10.  You have 4 chances to win!');
+
+  for (i = 0; i < 4; i++) {
+    guessingGameGuess = prompt('What number am I thinking of?');
+    guessingGameGuess = parseInt(guessingGameGuess);
+
+    if (guessingGameGuess === guessingGameAnswer) {
+      alert(`Great!  ${guessingGameGuess} is correct!  You got it in ${i+1} guesses!`);
+      i = 10;
+      numCorrect++;
+    } else if (guessingGameGuess > guessingGameAnswer) {
+      alert('Too high.  Try again');
+    } else if (guessingGameGuess < guessingGameAnswer) {
+      alert('Too low.  Try again');
+    }
+
+    if ((guessingGameGuess !== guessingGameAnswer) && i === 3){
+      alert(`Sorry, you ran out of chances.  The number I was thinking of is ${guessingGameAnswer}`);
+    }
+  }
+}
+
+
+// Guessing Movie Game FUNCTION
+function movie (){
+  // Guessing Movie Game: Local variables
+  let movieList = [
+    //  Answer      If correctly guessed
+    ['Star Wars', false],
+    ['Lord of the Rings', false],
+    ['The Matrix', false],
+  ];
+
+  let movieGuess = [];
+  let movieNumCorrect=0;
+  let i = 0;
+  let j = 0;
+
+  // Guessing Movie Game: initial prompt
+  alert('Guess my top 3 favorite movie series.  You have 6 chances');
+
+  // First loop: 6 guesses
+  for (i = 0; i < 6; i++) {
+    movieGuess[i] = prompt(`Guess #${i + 1}`);
+
+    // Second loop: compare against all 3 answers
+    for (j = 0; j < 3; j++) {
+      if (movieGuess[i] === movieList[j][0] && movieList[j][1] !== true) {
+        alert('Correct!');
+        movieNumCorrect++;
+        movieList[j][1] = true;
+        j = 3;
+      }
+    }
+
+    if (movieNumCorrect === 3){
+      i=6;
+    }
   }
 
-  // Format guess to y or n
-  guess = guess[0].toLowerCase();
+  alert(`My favorite movie series are ${movieList[0][0]}, ${movieList[1][0]}, and ${movieList[2][0]}`);
 
-  // check if answer is correct
-  if (guess === answer) {
-    alert(`The answer to "${question}" is correct!`);
-    // console.log(`The anwer to "${question}" is correct!`);
-    numCorrect++;
+  numCorrect += movieNumCorrect;
+}
+
+// Summary Function
+function summary(){
+  if (numCorrect >= 5){
+    alert(`${userName} - you got ${numCorrect} out of 9.  We must be best friends`);
+  } else if (numCorrect > 2){
+    alert(`${userName} - you got ${numCorrect} out of 9.  You did okay.  Let's have some lunch and get to know each other better`);
   } else {
-    alert(`The answer to "${question}" is incorrect.`);
-    // console.log(`The anwer to "${question}" is incorrect.`);
+    alert(`${userName} - you got ${numCorrect} out of 9.  Who are you again?`);
   }
 }
